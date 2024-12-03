0f5d4c64b29d3f63945bb668a46488bca22f5229
irem <iremberber0@gmail.com>
2024-12-03 20:04:22 +0300

    Create T-Python-101-1.py

---
+++ T-Python-101-1.py
@@ -0,0 +1,41 @@
+type(9.5)
+type("irem")
+type(10)
+
+""
+''
+
+123
+type(123)
+
+"a+b"
+"a" "b"
+"a" " b"
+"a" *3
+
+a = "geleceği-yazanlar"
+
+len(a)
+a.upper()
+a.lower()
+
+a.islower()
+a.isupper()
+
+a.replace("e","a")
+a.strip()
+
+a[2]
+a[5:10]
+
+x= 9
+y= "irem_berber"
+z= x*3
+
+toplama_1sayı = input()
+toplama_2sayı = input()
+
+print(int(toplama_1sayı) + int(toplama_2sayı))
+
+print("geleceği", "yazanlar", sep ="_")
+print()
